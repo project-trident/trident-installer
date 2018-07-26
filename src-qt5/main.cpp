490f5da18d324accb02a8f2ad8fe4056f742af1b
Ken Moore <ken@ixsystems.com>
2018-07-26 09:58:57 -0400

    Update the package.json to match the ISO build routine. Ensure the mouse cursor theme is setup before launching the UI

--- src-qt5/main.cpp
+++ src-qt5/main.cpp
@@ -5,16 +5,44 @@
 #include <QProcess>
 #include "mainUI.h"
 
+void setupCursorTheme(QString theme){
+  //Ensure dir existance but file deletion
+  QString path = "/usr/local/share/icons/default/index.theme"
+  if( QFile::exists(path)){ QFile::remove(path); }
+  else{
+    //Verify that the directory exists - or make it as needed
+    QDir dir;
+    dir.mkpath("/usr/local/share/icons/default");
+  }
+  //Assemble the file contents
+  QStringList contents;
+  contents << "[Icon Theme]"
+  contents << "Name=Default"
+  contents << "Comment=Default Cursor Theme"
+  contents << "Inherits="+theme
+  //Now create the file
+  QFile file(path);
+  if(file.open(QIODevice::WriteOnly | QIODevice::Text)){
+    QTextStream str(&file);
+    str << contents.join("\n");
+    str << "\n";
+    file.close();
+  }
+}
+
 int main(int argc, char ** argv)
 {
     //Ensure the screens are setup properly (mirror all at highest common resolution)
     QProcess::execute("lumina-xconfig", QStringList() << "--mirror-monitors");
 
     //Setup the environment for this app
-    // - icon theme
+    // - Icon theme
     QIcon::setThemeName("le-trident");
-    qputenv("QT_NO_GLIB","1");
+    // - Cursor Theme
+    setupCursorTheme("chameleon-pearl-regular");
 
+    //Regular performance improvements
+    qputenv("QT_NO_GLIB","1");
     //Create/start the application
     QApplication a(argc, argv);
     QFont font("noto-sans");
