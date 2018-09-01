4badd15eaf16284b97422a769def6cb529452f86
yater <vater@chelnok.de>
2018-09-01 16:24:01 +0200

    german translation (for the upcoming release)
    new installer; new translation; german is done!

--- src-qt5/i18n/tri-install_de.ts
+++ src-qt5/i18n/tri-install_de.ts
@@ -6,12 +6,12 @@
     <message>
         <location filename="../backend.cpp" line="288"/>
         <source>Required Package</source>
-        <translation type="unfinished"></translation>
+        <translation>Benötigtes Paket</translation>
     </message>
     <message>
         <location filename="../backend.cpp" line="299"/>
         <source>limit 1</source>
-        <translation type="unfinished"></translation>
+        <translation>Limit 1</translation>
     </message>
 </context>
 <context>
@@ -19,261 +19,262 @@
     <message>
         <location filename="../mainUI.ui" line="14"/>
         <source>MainWindow</source>
-        <translation type="unfinished"></translation>
+        <translation>HauptFenster</translation>
     </message>
     <message>
         <location filename="../mainUI.ui" line="86"/>
         <source>Collapse</source>
-        <translation type="unfinished"></translation>
+        <translation>Einklappen</translation>
     </message>
     <message>
         <location filename="../mainUI.ui" line="92"/>
         <source>Esc</source>
-        <translation type="unfinished"></translation>
+        <translation>Verlassen</translation>
     </message>
     <message>
         <location filename="../mainUI.ui" line="155"/>
         <source>Model</source>
-        <translation type="unfinished"></translation>
+        <translation>Model</translation>
     </message>
     <message>
         <location filename="../mainUI.ui" line="172"/>
         <source>Layout</source>
-        <translation type="unfinished"></translation>
+        <translatorcomment>Tastaturbelegung</translatorcomment>
+        <translation>Belegung</translation>
     </message>
     <message>
         <location filename="../mainUI.ui" line="185"/>
         <source>Variant</source>
-        <translation type="unfinished"></translation>
+        <translation>Variante</translation>
     </message>
     <message>
         <location filename="../mainUI.ui" line="208"/>
         <source>Test Here</source>
-        <translation type="unfinished"></translation>
+        <translation>Hier testen</translation>
     </message>
     <message>
         <location filename="../mainUI.ui" line="275"/>
         <source>Welcome to Project Trident!</source>
-        <translation type="unfinished"></translation>
+        <translation>Willkommen zum Projekt Trident!</translation>
     </message>
     <message>
         <location filename="../mainUI.ui" line="307"/>
         <source>Local Time</source>
-        <translation type="unfinished"></translation>
+        <translation>lokale Zeit</translation>
     </message>
     <message>
         <location filename="../mainUI.ui" line="330"/>
         <source>Time Zone</source>
-        <translation type="unfinished"></translation>
+        <translation>Zeitzone</translation>
     </message>
     <message>
         <location filename="../mainUI.ui" line="476"/>
         <source>Select Disk/Partition</source>
-        <translation type="unfinished"></translation>
+        <translation>Festplatte/Partition auswählen</translation>
     </message>
     <message>
         <location filename="../mainUI.ui" line="499"/>
         <source>Setup Accounts</source>
-        <translation type="unfinished"></translation>
+        <translation>Konten einrichten</translation>
     </message>
     <message>
         <location filename="../mainUI.ui" line="536"/>
         <source>Administrator Password</source>
-        <translation type="unfinished"></translation>
+        <translation>administratives Passwort</translation>
     </message>
     <message>
         <location filename="../mainUI.ui" line="545"/>
         <location filename="../mainUI.ui" line="703"/>
         <source>Password</source>
-        <translation type="unfinished"></translation>
+        <translation>Passwort</translation>
     </message>
     <message>
         <location filename="../mainUI.ui" line="570"/>
         <location filename="../mainUI.ui" line="728"/>
         <source>View Password</source>
-        <translation type="unfinished"></translation>
+        <translation>Passwort zeigen</translation>
     </message>
     <message>
         <location filename="../mainUI.ui" line="589"/>
         <location filename="../mainUI.ui" line="747"/>
         <source>Repeat Password</source>
-        <translation type="unfinished"></translation>
+        <translation>Passwort zeigen</translation>
     </message>
     <message>
         <location filename="../mainUI.ui" line="617"/>
         <location filename="../mainUI.ui" line="766"/>
         <source>Passwords do not match!!</source>
-        <translation type="unfinished"></translation>
+        <translation>Passwörter stimmen nicht überein!</translation>
     </message>
     <message>
         <location filename="../mainUI.ui" line="671"/>
         <source>User Account (more can be created later)</source>
-        <translation type="unfinished"></translation>
+        <translation>einfaches Konto (Mehr kann später erstellt werden.)</translation>
     </message>
     <message>
         <location filename="../mainUI.ui" line="683"/>
         <source>Name</source>
-        <translation type="unfinished"></translation>
+        <translation>Bezeichnung</translation>
     </message>
     <message>
         <location filename="../mainUI.ui" line="693"/>
         <source>Username</source>
-        <translation type="unfinished"></translation>
+        <translation>Bezeichnung der Benutzerin oder des Benutzers</translation>
     </message>
     <message>
         <location filename="../mainUI.ui" line="785"/>
         <source>Shell</source>
-        <translation type="unfinished"></translation>
+        <translation>Shell</translation>
     </message>
     <message>
         <location filename="../mainUI.ui" line="893"/>
         <source>Version</source>
-        <translation type="unfinished"></translation>
+        <translation>Version</translation>
     </message>
     <message>
         <location filename="../mainUI.ui" line="898"/>
         <source>Description</source>
-        <translation type="unfinished"></translation>
+        <translation>Beschreibung</translation>
     </message>
     <message>
         <location filename="../mainUI.ui" line="928"/>
         <source>Select Packages to Install</source>
-        <translation type="unfinished"></translation>
+        <translation>Pakete zum Installieren auswählen</translation>
     </message>
     <message>
         <location filename="../mainUI.ui" line="1049"/>
         <source>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;&lt;span style=&quot; font-weight:600;&quot;&gt;Make sure this is correct!&lt;/span&gt; Once you start the installation, it will overwrite any data on the designated disks/partitions!&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</source>
-        <translation type="unfinished"></translation>
+        <translation>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;&lt;span style=&quot; font-weight:600;&quot;&gt;Stellen Sie sicher, dass dies korrekt ist!&lt;/span&gt; Sobald Sie die Installation gestartet haben, wird sie jegliche Daten auf den angegebenen Festplatten/Partitionen überschreiben!&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</translation>
     </message>
     <message>
         <location filename="../mainUI.ui" line="1068"/>
         <source>Review: Summary of Changes</source>
-        <translation type="unfinished"></translation>
+        <translation>Übersicht: Zusammenfassung der Änderungen</translation>
     </message>
     <message>
         <location filename="../mainUI.ui" line="1089"/>
         <source>Tab 1</source>
-        <translation type="unfinished"></translation>
+        <translation>Tab 1</translation>
     </message>
     <message>
         <location filename="../mainUI.ui" line="1101"/>
         <source>Install Slide 1</source>
-        <translation type="unfinished"></translation>
+        <translation>Folie 1 der Installation</translation>
     </message>
     <message>
         <location filename="../mainUI.ui" line="1107"/>
         <source>Tab 2</source>
-        <translation type="unfinished"></translation>
+        <translation>Tab 2</translation>
     </message>
     <message>
         <location filename="../mainUI.ui" line="1119"/>
         <source>Install Slide 2</source>
-        <translation type="unfinished"></translation>
+        <translation>Folie 2 der Installation</translation>
     </message>
     <message>
         <location filename="../mainUI.ui" line="1152"/>
         <source>Installing...</source>
-        <translation type="unfinished"></translation>
+        <translation>Installieren...</translation>
     </message>
     <message>
         <location filename="../mainUI.ui" line="1174"/>
         <source>Installation Finished</source>
-        <translation type="unfinished"></translation>
+        <translation>Installation beendet</translation>
     </message>
     <message>
         <location filename="../mainUI.ui" line="1197"/>
         <source>TextLabel</source>
-        <translation type="unfinished"></translation>
+        <translation>TextBezeichnung</translation>
     </message>
     <message>
         <location filename="../mainUI.ui" line="1227"/>
         <source>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;&lt;span style=&quot; font-weight:600;&quot;&gt;Note:&lt;/span&gt; Please remove the installation medium (dvd or usb) before powering on your system again.&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</source>
-        <translation type="unfinished"></translation>
+        <translation>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;&lt;span style=&quot; font-weight:600;&quot;&gt;Hinweis:&lt;/span&gt; Bitte entfernen Sie das Medium von dem die Installation stattfand (DVD or USB) bevor Sie Ihr System wieder anschalten.&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</translation>
     </message>
     <message>
         <location filename="../mainUI.ui" line="1275"/>
         <source>Page %v/%m</source>
-        <translation type="unfinished"></translation>
+        <translation>Seite %v/%m</translation>
     </message>
     <message>
         <location filename="../mainUI.ui" line="1295"/>
         <source>Previous</source>
-        <translation type="unfinished"></translation>
+        <translation>vorherig</translation>
     </message>
     <message>
         <location filename="../mainUI.ui" line="1302"/>
         <source>Alt+P</source>
-        <translation type="unfinished"></translation>
+        <translation>Alt+P</translation>
     </message>
     <message>
         <location filename="../mainUI.ui" line="1312"/>
         <source>Next</source>
-        <translation type="unfinished"></translation>
+        <translation>nächste</translation>
     </message>
     <message>
         <location filename="../mainUI.ui" line="1319"/>
         <source>Alt+N</source>
-        <translation type="unfinished"></translation>
+        <translation>Alt+N</translation>
     </message>
     <message>
         <location filename="../mainUI.ui" line="1329"/>
         <source>Start Install</source>
-        <translation type="unfinished"></translation>
+        <translation>Installation starten</translation>
     </message>
     <message>
         <location filename="../mainUI.ui" line="1343"/>
         <source>Reboot Now</source>
-        <translation type="unfinished"></translation>
+        <translation>jetzt Neustarten</translation>
     </message>
     <message>
         <location filename="../mainUI.ui" line="1368"/>
         <source>toolBar</source>
-        <translation type="unfinished"></translation>
+        <translation>WerkzeugLeiste</translation>
     </message>
     <message>
         <location filename="../mainUI.ui" line="1404"/>
         <source>System</source>
-        <translation type="unfinished"></translation>
+        <translation>System</translation>
     </message>
     <message>
         <location filename="../mainUI.ui" line="1407"/>
         <source>System Information</source>
-        <translation type="unfinished"></translation>
+        <translation>Information zum System</translation>
     </message>
     <message>
         <location filename="../mainUI.ui" line="1422"/>
         <source>Keyboard</source>
-        <translation type="unfinished"></translation>
+        <translation>Tastatur</translation>
     </message>
     <message>
         <location filename="../mainUI.ui" line="1425"/>
         <source>Keyboard Settings</source>
-        <translation type="unfinished"></translation>
+        <translation>Einstellungen der Tastatur</translation>
     </message>
     <message>
         <location filename="../mainUI.ui" line="1437"/>
         <source>Locale</source>
-        <translation type="unfinished"></translation>
+        <translation>Region</translation>
     </message>
     <message>
         <location filename="../mainUI.ui" line="1440"/>
         <source>Localization</source>
-        <translation type="unfinished"></translation>
+        <translation>Regionalisierung</translation>
     </message>
     <message>
         <location filename="../mainUI.cpp" line="129"/>
         <source>Unpartitioned free space</source>
-        <translation type="unfinished"></translation>
+        <translation>nicht partitionierter freier Platz</translation>
     </message>
     <message>
         <location filename="../mainUI.cpp" line="204"/>
         <source>Verify Hard Disk</source>
-        <translation type="unfinished"></translation>
+        <translation>Festplatte prüfen</translation>
     </message>
     <message>
         <location filename="../mainUI.cpp" line="204"/>
         <source>This will destroy any existing partitions and all data on the selected hard drive. Are you sure you want to continue? </source>
-        <translation type="unfinished"></translation>
+        <translation>Dies wird auf der ausgewählten Festplatte jegliche bestehende Partitionen und alle Daten zerstören. Sind Sie sich sicher fortfahen zu wollen? </translation>
     </message>
 </context>
 </TS>
