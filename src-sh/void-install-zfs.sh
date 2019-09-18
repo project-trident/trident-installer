4e34a795f7e0d3b17136e824db612f3953abca51
Ken Moore <ken@ixsystems.com>
2019-09-18 15:15:00 -0400

    Fix up the chroot usage in teh install script

--- src-sh/void-install-zfs.sh
+++ src-sh/void-install-zfs.sh
@@ -9,7 +9,8 @@ REPO="http://alpha.de.repo.voidlinux.org/current/musl"
 PACKAGES=""
 PACKAGES_CHROOT="iwd wpa_supplicant dhcpcd bluez linux-firmware foomatic-db-nonfree vlc phototonic trojita telegram-desktop falkon lynx qterminal openvpn git pianobar w3m ntfs-3g fuse-exfat simple-mtpfs fish-shell zsh x264 libdvdcss gutenprint foomatic-db hplip tor nano xorg lumina dhclient"
 SERVICES_ENABLED="dbus sshd dhcpcd dhclient cupsd wpa_supplicant"
-
+MNT="/mnt"
+CHROOT="chroot ${MNT}/"
 ## Some important packages
 ## nano xorg lumina iwd wpa_supplicant dhcpcd bluez linux-firmware-network
 
@@ -47,29 +48,29 @@ echo "Export the pool"
 echo "zpool set bootfs=rpool/ROOT/voidlinux_1 <pool_name>"
 zpool export ${ZPOOL}
 echo
-echo "Import the pool below /mnt:"
-echo "zpool import -R /mnt ${ZPOOL}"
-zpool import -R /mnt ${ZPOOL}
+echo "Import the pool below ${MNT}:"
+echo "zpool import -R ${MNT} ${ZPOOL}"
+zpool import -R ${MNT} ${ZPOOL}
 echo
 echo "making neccesary directories" 
-echo "mkdir -p /mnt/{boot/grub,dev,proc,run,sys}"
+echo "mkdir -p ${MNT}/{boot/grub,dev,proc,run,sys}"
 dirs="boot/grub dev etc proc run sys"
 for dir in ${dirs}
 do
-  mkdir -p /mnt/${dir}
+  mkdir -p ${MNT}/${dir}
 done
 echo
 echo " mount drive {/dev/sda} to /boot/grub"
-mount $BOOTDRIVE /mnt/boot/grub
+mount $BOOTDRIVE ${MNT}/boot/grub
 echo
 echo "mounting necessary directories"
-mount --rbind /dev /mnt/dev
-mount --rbind /proc /mnt/proc
-mount --rbind /run /mnt/run
-mount --rbind /sys /mnt/sys
+mount --rbind /dev ${MNT}/dev
+mount --rbind /proc ${MNT}/proc
+mount --rbind /run ${MNT}/run
+mount --rbind /sys ${MNT}/sys
 echo
 echo " mount drive {/dev/sda} to /boot/grub"
-mount $BOOTDRIVE /mnt/boot/grub
+mount $BOOTDRIVE ${MNT}/boot/grub
 echo
 echo "creating /home to snapshot"
 zfs create -o compression=lz4 			${ZPOOL}/home
@@ -82,71 +83,61 @@ zfs create -o compression=lz4	          	${ZPOOL}/var/mail
 echo
 echo "Installing MUSL voidlinux, before chroot into it"
 xbps-install -S
-XBPS_ARCH=x86_64-musl xbps-install -S -y --repository=${REPO}  -r /mnt base-system grub ${PACKAGES}
+XBPS_ARCH=x86_64-musl xbps-install -S -y --repository=${REPO}  -r ${MNT} base-system grub ${PACKAGES}
 echo
 echo "copying a valid resolv.conf into directory, before chroot to get to the new install"
 if [ -e "/etc/resolv.conf" ] ; then
   #Copy the current host resolv.conf (assume it is working)
-  cp /etc/resolv.conf /mnt/etc/resolv.conf
+  cp /etc/resolv.conf ${MNT}/etc/resolv.conf
 fi
 #Now inject a couple always-working DNS nameservers into the end of resolv.conf
-echo "8.8.8.8" >> /mnt/etc/resolv.conf
-echo "8.8.4.4" >> /mnt/etc/resolv.conf
+echo "8.8.8.8" >> ${MNT}/etc/resolv.conf
+echo "8.8.4.4" >> ${MNT}/etc/resolv.conf
 
 echo "CHROOT into mount and finish setting up"
 
-chroot /mnt/ /bin/bash
+#chroot ${MNT}/ /bin/bash
 echo "setting up /"
-chown root:root /
-chmod 755 /
-passwd root
+${CHROOT} chown root:root /
+${CHROOT} chmod 755 /
+#passwd root
 echo
 
 echo "sync repo, add additional repo, and then re-sync"
-xbps-install -S
-xbps-install -y void-repo-nonfree 
-xbps-install -S
+${CHROOT} xbps-install -S
+${CHROOT} xbps-install -y void-repo-nonfree 
+${CHROOT} xbps-install -S
 
 echo
 echo "NOW install zfs and other packages which make config changes on install"
-xbps-install zfs -y ${PACKAGES_CHROOT}
+${CHROOT} xbps-install zfs -y ${PACKAGES_CHROOT}
 echo
-
-echo
-echo "Check if grub-probe / outputs zfs, else hack /usr/sbin/grub-mkconfig and insert values of"
-echo "GRUB_DEVICE=/dev/sda2 and GRUB_DEVICE_BOOT=/dev/sda2 directly."
-echo
-if [ "zfs" != $(grub-probe /) ] ; then
-  echo "ERROR: Could not verify ZFS nature of /"
-  exit 1
-fi  
-grub-install ${BOOTDEVICE}
 
 echo
 echo "making sure we have this file /etc/zfs/zpool.cache" 
-zpool set cachefile=/etc/zfs/zpool.cache ${ZPOOL}
+${CHROOT} zpool set cachefile=/etc/zfs/zpool.cache ${ZPOOL}
 
 echo
 echo "Auto-enabling services"
 for service in  ${SERVICES_ENABLED}
 do
   echo " -> ${service}"
-  ln -s /etc/sv/${service} /var/service/${service}
+  ${CHROOT} ln -s /etc/sv/${service} /var/service/${service}
 done
 
 echo
 echo "Fix dracut and kernel config, then update grub"
-echo hostonly=yes >> /etc/dracut.conf
-xbps-reconfigure -f linux5.2
+echo hostonly=yes >> ${MNT}/etc/dracut.conf
+${CHROOT} xbps-reconfigure -f linux5.2
 #Now reinstall grub on the boot device after the reconfiguration
-if [ "zfs" != $(grub-probe /) ] ; then
+if [ "zfs" != $(${CHROOT} grub-probe /) ] ; then
   echo "ERROR: Could not verify ZFS nature of /"
   exit 1
 fi  
-grub-install ${BOOTDEVICE}
+${CHROOT} grub-install ${BOOTDEVICE}
 
 echo "=============="
-echo "FINAL STEP: edit /etc/rc.conf to uncomment info as necessary"
-echo "1. nano /etc/rc.conf"
+echo "FINAL STEP: edit ${MNT}/etc/rc.conf to uncomment info as necessary"
+echo "1. vi ${MNT}/etc/rc.conf"
 echo "2. reboot when ready"
 echo "=============="
