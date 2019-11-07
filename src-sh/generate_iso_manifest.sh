26b3df888df784e71a354c1f30898bab9a6e6537
Ken Moore <ken@ixsystems.com>
2019-11-06 20:18:46 -0500

    Add a new script for signing iso files and generating a manifest.

---
+++ src-sh/generate_iso_manifest.sh
@@ -0,0 +1,59 @@
+#!/bin/sh
+
+_isofile="${1}"
+_licfile="${2}"
+_outdir="${3}"
+_version="${4}"
+
+isopath="${_isofile}"
+
+isoname=$(basename "${_isofile}")
+isosize=`ls -lh "${isopath}" | cut -w -f 5`
+sigfile="${isoname}.sig.sha512"
+md5_file="${isoname}.md5"
+sha_file="${isoname}.sha256"
+
+
+if [ "${_isofile}" != "${_outdir}/${isoname}" ] ; then
+  if [ -f "${_outdir}/${isoname}" ] ; then
+    rm "${_outdir}/${isoname}"
+  fi
+  cp "${_isofile}" "${_outdir}/${isoname}"
+fi
+
+isopath="${_outdir}/${isoname}"
+sign_iso(){
+  openssl dgst -sha512 -sign "${_licfile}" -out "${_outdir}/${sigfile}" "${isopath}"
+}
+
+generate_checksums(){
+  #MD5
+  md5 "${isopath}" > "${_outdir}/${md5_file}"
+  md5_sum=`cat "${_outdir}/${md5_file}" | rev | cut -w -f 1 | rev`
+  #SHA256
+  sha256 "${isopath}" > "${_outdir}/${sha_file}"
+  sha_sum=`cat "${_outdir}/${sha_file}" | rev | cut -w -f 1 | rev`
+}
+
+generate_manifest(){
+echo "{
+\"iso_file\" : \"${isoname}\",
+\"iso_size\" : \"${isosize}\",
+\"signature_file\" : \"${sigfile}\",
+\"md5_file\" : \"${md5_file}\",
+\"md5\" : \"${md5_sum}\",
+\"sha256_file\" : \"${sha_file}\",
+\"sha256\" : \"${sha_sum}\",
+\"build_date\" : \"$(date -j '+%b %d, %Y')\",
+\"build_date_time_t\" : \"$(date -j '+%s')\",
+\"version\" : \"${_version}\"
+}
+" > "${_outdir}/manifest.json"
+}
+
+if [ ! -d "${_outdir}" ] ; then
+  mkdir -p "${_outdir}"
+fi
+sign_iso
+generate_checksums
+generate_manifest
