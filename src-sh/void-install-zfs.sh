d3fe98539887c2ae968733eafeb069038720f7df
Ken Moore <ken@ixsystems.com>
2020-01-02 15:22:21 -0500

    Add cifs-utils to the server packages

--- src-sh/void-install-zfs.sh
+++ src-sh/void-install-zfs.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-SERVER_PACKAGES="iwd nano git jq zsh fish-shell wireguard pam_zfscrypt bluez nftables dcron autofs"
+SERVER_PACKAGES="iwd nano git jq zsh fish-shell wireguard pam_zfscrypt bluez nftables dcron autofs cifs-utils"
 LITE_PACKAGES="${SERVER_PACKAGES} noto-fonts-ttf xorg-minimal xf86-video-fbdev lumina qterminal compton hicolor-icon-theme trident-icons xrandr qt5-svg wpa-cute libdvdcss gutenprint ntfs-3g fuse-exfat simple-mtpfs pavucontrol"
 FULL_PACKAGES="${LITE_PACKAGES} telegram-desktop vlc firefox trojita pianobar libreoffice falkon spotify"
 
