c5b396fb62528da9d8099906744335d808b66b34
Ken Moore <ken@ixsystems.com>
2020-01-18 16:02:40 -0500

    Fix another package name in the full desktop list. all set and working again

--- src-sh/void-install-zfs.sh
+++ src-sh/void-install-zfs.sh
@@ -2,7 +2,7 @@
 
 SERVER_PACKAGES="iwd nano git jq zsh fish-shell wireguard bluez bluez-alsa nftables dcron autofs cifs-utils firejail openvpn openntpd"
 LITE_PACKAGES="${SERVER_PACKAGES} noto-fonts-ttf xorg-fonts xorg-minimal mesa-dri xf86-video-fbdev lumina qterminal qsudo compton hicolor-icon-theme trident-icons xrandr qt5-svg wpa-cute libdvdcss ntfs-3g fuse-exfat simple-mtpfs pulseaudio pavucontrol gst-libav sddm"
-FULL_PACKAGES="${LITE_PACKAGES} telegram-desktop vlc firefox trojita pianobar libreoffice cups foomatic-db foomatic-engine cups-filters"
+FULL_PACKAGES="${LITE_PACKAGES} telegram-desktop vlc firefox trojita pianobar libreoffice cups foomatic-db foomatic-db-engine cups-filters"
 
 SERVICES_ENABLED="dbus dhcpcd cupsd wpa_supplicant bluetoothd bluez-alsa acpid nftables dcron autofs openntpd sddm"
 
