dafec6b7a4cc7271a9cffd6984580f817a5b24eb
Ken Moore <ken@ixsystems.com>
2020-02-05 14:40:07 -0500

    Update the ISO install startup to support multiple installer script versions.

--- src-sh/void-install-zfs.sh
+++ src-sh/void-install-zfs.sh
@@ -5,7 +5,7 @@ SERVER_PACKAGES="zsh fish-shell firejail openvpn neofetch sl wget trident-core"
 LITE_PACKAGES="${SERVER_PACKAGES} wpa-cute ntfs-3g fuse-exfat simple-mtpfs trident-desktop setxkbmap"
 FULL_PACKAGES="${LITE_PACKAGES} telegram-desktop vlc firefox trojita pianobar libreoffice cups foomatic-db foomatic-db-engine cups-filters"
 
-SERVICES_ENABLED="dbus dhcpcd cupsd wpa_supplicant bluetoothd bluez-alsa acpid nftables dcron autofs openntpd sddm"
+SERVICES_ENABLED="dbus dhcpcd cupsd wpa_supplicant bluetoothd acpid nftables dcron autofs openntpd sddm"
 
 if [ "${1}" = "-h" ] || [ "${1}" = "help" ] || [ "${1}" = "--help" ] ; then
 echo "Project Trident Installer
